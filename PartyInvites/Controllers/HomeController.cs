b30af5e8da0a10caf897ec33d35686d9452e4b41
Yaroslav Prishedko <yaroslav05042005@gmail.com>
2021-01-13 22:48:46 +0300

    Добавьте файлы проекта.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            // Render a template
+            return View("RsvpForm");
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // Save guest response
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // Entered credentials are not valid
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            // View guest responses where WillAttend == true
+            return View(Repository.Responses.Where(row => row.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required (ErrorMessage = "Please, enter your name.")]
+        public string Name { get; set; }
+
+
+        [Required (ErrorMessage = "Please, enter your email adress.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please, enter a valid email address.")]
+        public string Email { get; set; }
+
+
+        [Required (ErrorMessage = "Please enter your phone number.")]
+        public string Phone { get; set; }
+
+        [Required (ErrorMessage = "Please specify whether you will attend or not.")]
+        public bool? WillAttend { get; set; }
+
+        public static int i = 0;
+    }
+}
